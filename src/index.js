fe601ace1da908c476830e723bfe04a66167a184
Rodrigo Sousa <rodrigoasferreira@protonmail.com>
2024-07-27 19:00:14 -0300

    Fix form button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import "./styles.css";
 
+const addTaskBtn = document.querySelector(".btn-add");
+const formContainer = document.querySelector(".form-container");
+const formDialog = document.querySelector("#form-dialog");
+
 console.log("is it working???");
-console.log("KASSSSSINÃAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOOOOOOOOOOOOOOO");
+
+addTaskBtn.addEventListener("click", (button) => {
+  formContainer.reset();
+  formDialog.showModal();
+  console.log("mady musico agr kkk");
+});
+
+formDialog.addEventListener("click", () => {
+  formDialog.close();
+});
+
+formContainer.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
